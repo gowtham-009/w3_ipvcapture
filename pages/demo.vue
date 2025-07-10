1f8341b66f89df15835493f4a678a75c02a6957a
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-07-10 15:37:41 +0530

    uj

--- pages/demo.vue
+++ pages/demo.vue
@@ -341,24 +341,34 @@ function handleLocationError(error) {
 }
 
 function requestLocation() {
+  // Must be triggered directly from button click to work on iOS
   showLocationAlert.value = false;
   locationLoading.value = true;
 
-  // Must be triggered by a button click on iOS
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      handleLocationSuccess(position);
-    },
-    (err) => {
-      handleLocationError(err);
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 10000,
-      maximumAge: 0
-    }
-  );
+  // Use direct sync call
+  setTimeout(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        latitude.value = position.coords.latitude;
+        longitude.value = position.coords.longitude;
+        locationEnabled.value = true;
+        locationLoading.value = false;
+      },
+      (error) => {
+        locationEnabled.value = false;
+        locationLoading.value = false;
+        showLocationAlert.value = true;
+        console.error('Location error (iOS):', error.message);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+      }
+    );
+  }, 0); 
 }
+
 
 function onImageCaptured(imageData) {
   imageCaptured.value = imageData;
